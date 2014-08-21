66bc202122855a5cbf5c6f9ebb3788fb120dd09a
lszymkowiak <ryzy82@gmail.com>
2014-08-21 19:20:51 +0200

    removed unavailable components, grunt tasks cleanup

--- gruntfile.js
+++ gruntfile.js
@@ -6,19 +6,20 @@ module.exports = function(grunt){
         pkg: grunt.file.readJSON('package.json'),
 
         clean: {
-            all: ["public/*"]
+            all: ["public/*"],
         },
 
-        run_grunt: {
-            options: {
-                minimumFiles: 1,
-            },
-            jquery_ui: {
+        bower: {
+            install: {
                 options: {
-                    log: true,
-                },
-                src: ['gruntfile-jquery-ui.js']
-            },
+                    cleanTargetDir: true,
+                    targetDir: './bower_components',
+                    bowerOptions: {
+                        forceLatest: true,
+                        production: true,
+                    }
+                }
+            }
         },
 
         copy: {
@@ -28,17 +29,9 @@ module.exports = function(grunt){
                 ]
             },
 
-            jquery_cycle: {
+            angular: {
                 files: [
-                    {expand: true, cwd: 'bower_components/jquery-cycle2/build/', src: ['jquery.cycle2.js'], dest: 'public/js/', filter: 'isFile'},
-                ]
-            },
-
-            jquery_ui: {
-                files: [
-                    {expand: true, cwd: 'bower_components/jquery.ui/dist/', src: ['jquery-ui.js', 'jquery-ui-i18n.js'], dest: 'public/js/', filter: 'isFile'},
-                    {expand: true, cwd: 'bower_components/jquery.ui/dist/', src: ['jquery-ui.css', 'jquery-ui.min.css'], dest: 'public/css/', filter: 'isFile'},
-                    {expand: true, cwd: 'bower_components/jquery.ui/dist/gfx/', src: ['**'], dest: 'public/gfx/ui/', filter: 'isFile'},
+                    {expand: true, cwd: 'bower_components/angular/', src: ['angular.js'], dest: 'public/js/', filter: 'isFile'},
                 ]
             },
 
@@ -51,22 +44,6 @@ module.exports = function(grunt){
                 ]
             },
 
-            lightbox: {
-                files: [
-                    {expand: true, cwd: 'bower_components/lightbox/js/', src: ['lightbox.js'], dest: 'public/js/', filter: 'isFile'},
-                    {expand: true, cwd: 'bower_components/lightbox/css/', src: ['lightbox.css'], dest: 'public/css/', filter: 'isFile'},
-                    {expand: true, cwd: 'bower_components/lightbox/img/', src: ['close.png', 'loading.gif', 'next.png', 'prev.png'], dest: 'public/gfx/lightbox/', filter: 'isFile'},
-                ]
-            },
-
-            supersized: {
-                files: [
-                    {expand: true, cwd: 'bower_components/supersized/core/js/', src: ['supersized.core.3.2.1.js'], dest: 'public/js/', filter: 'isFile'},
-                    {expand: true, cwd: 'bower_components/supersized/core/css/', src: ['supersized.core.css'], dest: 'public/css/', filter: 'isFile'},
-                    {expand: true, cwd: 'bower_components/supersized/core/img/', src: ['bg-black.png', 'progress.gif', 'supersized-logo.png'], dest: 'public/gfx/supersized/', filter: 'isFile'},
-                ]
-            },
-
             fontawesome: {
                 files: [
                     {expand: true, cwd: 'bower_components/fontawesome/css/', src: ['font-awesome.css'], dest: 'public/css/', filter: 'isFile'},
@@ -74,35 +51,39 @@ module.exports = function(grunt){
                 ]
             },
 
-            uploadify: {
+            holder: {
                 files: [
-                    {expand: true, cwd: 'bower_components/uploadify/', src: ['jquery.uploadify.js'], dest: 'public/js/', filter: 'isFile'},
-                    {expand: true, cwd: 'bower_components/uploadify/', src: ['uploadify.css'], dest: 'public/css/', filter: 'isFile'},
-                    {expand: true, cwd: 'bower_components/uploadify/', src: ['uploadify-cancel.png'], dest: 'public/gfx/uploadify/', filter: 'isFile'},
-                    {expand: true, cwd: 'bower_components/uploadify/', src: ['uploadify.swf'], dest: 'public/gfx/uploadify/', filter: 'isFile'},
+                    {expand: true, cwd: 'bower_components/holderjs/', src: ['holder.js'], dest: 'public/js/', filter: 'isFile'},
                 ]
             },
+
+            jquery_cycle: {
+                files: [
+                    {expand: true, cwd: 'bower_components/jquery-cycle2/build/', src: ['jquery.cycle2.js'], dest: 'public/js/', filter: 'isFile'},
+                ]
+            },
+
+            lightbox: {
+                files: [
+                    {expand: true, cwd: 'bower_components/lightbox/js/', src: ['lightbox.js'], dest: 'public/js/', filter: 'isFile'},
+                    {expand: true, cwd: 'bower_components/lightbox/css/', src: ['lightbox.css'], dest: 'public/css/', filter: 'isFile'},
+                    {expand: true, cwd: 'bower_components/lightbox/img/', src: ['close.png', 'loading.gif', 'next.png', 'prev.png'], dest: 'public/gfx/lightbox/', filter: 'isFile'},
+                ]
+            }
         },
 
         rename: {
+            fontawesome: {
+                files: [
+                    {src: ['public/css/font-awesome.css'], dest: 'public/css/fontawesome.css'},
+                ]
+            },
+
             jquery_cycle: {
                 files: [
                     {src: ['public/js/jquery.cycle2.js*'], dest: 'public/js/jquery-cycle2.js'},
                 ]
             },
-
-            supersized: {
-                files: [
-                    {src: ['public/js/supersized.*'], dest: 'public/js/supersized.js'},
-                    {src: ['public/css/supersized.*'], dest: 'public/css/supersized.css'},
-                ]
-            },
-
-            uploadify: {
-                files: [
-                    {src: ['public/js/jquery.uploadify.js'], dest: 'public/js/jquery-uploadify.js'},
-                ]
-            }
         },
 
         replace: {
@@ -137,30 +118,7 @@ module.exports = function(grunt){
                         to:     '../gfx/lightbox/'
                     },
                 ]
-            },
-
-
-            supersized : {
-                src: ['public/css/supersized.css'],
-                dest: 'public/css/supersized.css',
-                replacements: [
-                    {
-                        from:   '../img/',
-                        to:     '../gfx/supersized/'
-                    },
-                ]
-            },
-
-            uploadify: {
-                src: ['public/css/uploadify.css'],
-                dest: 'public/css/uploadify.css',
-                replacements: [
-                    {
-                        from:   '../img/',
-                        to:     '../gfx/uploadify/'
-                    },
-                ]
-            },
+            }
         },
 
         uglify: {
@@ -169,58 +127,72 @@ module.exports = function(grunt){
                     drop_console: true
                 }
             },
-
             all: {
                 files: [{
                     expand: true,
                     cwd: 'public/js/',
                     src: ['*.js', '!*.min.js'],
-                    dest: 'public/js/min/',
+                    dest: 'public/js/',
                     ext: '.min.js'
+                }]
+            },
+        },
+
+        cssmin: {
+            all: {
+                files: [{
+                    expand: true,
+                    cwd: 'public/css/',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'public/css/',
+                    ext: '.min.css'
                 }]
             },
         },
 
     });
 
+    // update
+    grunt.registerTask('update', ['clean']);
+
+    // jquery
+    grunt.registerTask('jquery', ['copy:jquery']);
+
+    // angular
+    grunt.registerTask('angular', ['copy:angular']);
+
+    // bootstrap
+    grunt.registerTask('bootstrap', ['copy:bootstrap', 'replace:bootstrap']);
+
+    // fontawesome
+    grunt.registerTask('fontawesome', ['copy:fontawesome', 'rename:fontawesome']);
+
+    // holder
+    grunt.registerTask('holder', ['copy:holder']);
+
+    // cycle
+    grunt.registerTask('cycle', ['copy:jquery_cycle', 'rename:jquery_cycle']);
+
+    // lightbox
+    grunt.registerTask('lightbox', ['copy:lightbox', 'replace:lightbox_js', 'replace:lightbox_css']);
+
+    // min
+    grunt.registerTask('min', ['uglify', 'cssmin']);
+
+    // default
     grunt.registerTask('default', [
+        'bower',
         'clean',
-
-        // jquery
-        'copy:jquery',
-
-        // jquery_cycle
-        'copy:jquery_cycle',
-        'rename:jquery_cycle',
-
-        // // jquery_ui
-        'run_grunt:jquery_ui',
-        'copy:jquery_ui',
-
-        // bootstrap
-        'copy:bootstrap',
-        'replace:bootstrap',
-
-        // lightbox
-        'copy:lightbox',
-        'replace:lightbox_js',
-        'replace:lightbox_css',
-
-        // supersized
-        'copy:supersized',
-        'rename:supersized',
-        'replace:supersized',
-
-        // fontawesome
-        'copy:fontawesome',
-
-        // uploadify
-        'copy:uploadify',
-        'rename:uploadify',
-        'replace:uploadify',
-
-        // uglify
-        'uglify:all',
+        'jquery',
+        'angular',
+        'bootstrap',
+        'fontawesome',
+        'holder',
+        'cycle',
+        'lightbox',
+        'min'
     ]);
+
+
 
 };
